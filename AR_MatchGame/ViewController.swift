cccdbce8218a05e69f6a0f16d9fe66139be2086a
shenmali <muhalishen@gmail.com>
2023-06-06 14:00:17 +0300

    Initial Commit

---
+++ AR_MatchGame/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  AR_MatchGame
+//
+//  Created by Ali on 6.06.2023.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
